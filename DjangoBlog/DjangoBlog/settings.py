c56de0528029aec39404031357254ac33f3c8368
wangxing <wang_xing@nec.cn>
2019-11-08 18:32:40 +0900

    feat:create blog.context_processors.py

--- DjangoBlog/DjangoBlog/settings.py
+++ DjangoBlog/DjangoBlog/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'blog.context_processors.sidebar',  #为所有模板文件添加上下文渲染器
             ],
         },
     },
---
+++ DjangoBlog/blog/context_processors.py
@@ -0,0 +1,23 @@
+#from django.conf import settings
+from .models import Tag, Category, Article
+
+"""
+blog app上下文渲染器
+"""
+
+#导航栏和右边栏的上下文渲染器
+def sidebar(request):
+    # 所有标签
+    tag_list = Tag.objects.all()
+
+    # 所有文章分类
+    category_list = Category.objects.all() 
+
+    # 文章阅读量排行榜
+    article_ranklist = Article.objects.all().order_by('-view')[0:6]  
+    
+    return {
+        'category_list': category_list,
+        'article_rank': article_ranklist,
+        'tag_list': tag_list 
+    }
