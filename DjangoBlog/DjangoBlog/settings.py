deeebf57ffb79e3c98518aacbc827cd84a8dc106
王星 <344721710@qq.com>
2019-11-09 14:38:35 +0800

    feat: add mysql's settings.

--- DjangoBlog/DjangoBlog/settings.py
+++ DjangoBlog/DjangoBlog/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'DjangoBlog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',         #sqlite3的配置
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),   #sqlite3的配置
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangoblog',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',        
     }
 }
 
