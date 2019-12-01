24abce8f8f33d978c10fabfb9e55fadb5399138e
王星 <344721710@qq.com>
2019-12-01 18:11:35 +0800

    feat:完成 更新个人信息功能，添加从前端上传个人头像的功能

--- DjangoBlog/users/views.py
+++ DjangoBlog/users/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 #from django.contrib.auth.forms import UserCreationForm #django默认注册用户的form
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required #只有已经登录的用户才能访问视图
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 
 # Create your views here.
@@ -24,4 +24,23 @@ def register(request):
 #用户必须login之后才能进入这个视图
 @login_required
 def profile(request):
-    return render(request,'users/profile.html')
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, 
+                                    request.FILES, 
+                                    instance=request.user.profile)
+        if u_form.is_valid() and p_form.is_valid():
+            u_form.save()
+            p_form.save()
+            messages.success(request,f'个人信息更新成功！')
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context ={
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request,'users/profile.html',context)
