151226dc491ced30970fbec30e985daf88957da3
王星 <344721710@qq.com>
2019-12-01 18:42:25 +0800

    feat: 对于上传的头像，只保存缩略图到文件系统

--- DjangoBlog/users/models.py
+++ DjangoBlog/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image  # 创建缩略图用
 
 class Profile(models.Model):
     #一对一，一个profile对应一个User，并指明用户删除同时删除profile，
@@ -10,7 +11,16 @@ class Profile(models.Model):
     #上传路径到：profile_pics
     image = models.ImageField(default='default.jpg',upload_to='profile_pics')
 
-    #print用
+    # print用
     def __str__(self):
         return f'{self.user.username} Profile'
 
+    # 重写父类（models.Model）的save的方法，用来保存上传图片的缩略图
+    def save(self):
+        super().save()
+        img = Image.open(self.image.path)
+        if img.height > 300 or img.width > 300:            
+            output_size = (300, 300) 
+            img.thumbnail(output_size)          
+            img.save(self.image.path)
+            
