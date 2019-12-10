42024be5473760fa56afc261fd21f88e28dffc56
wangxing <wang_xing@nec.cn>
2019-12-10 18:41:05 +0900

    feat: 使用通用视图代替传统函数视图。

--- DjangoBlog/blog/urls.py
+++ DjangoBlog/blog/urls.py
@@ -7,7 +7,10 @@ from .views import (
     ArticleDetailView,
     ArticleCreateView,
     ArticleUpdateView,
-    ArticleDeleteView
+    ArticleDeleteView,
+    UserArticleListView,
+    CategoryListView,
+    TagListView
 )
 
 app_name = 'blog'  #定义应用程序的命名空间(访问路由地时候可以用“应用命名空间：路由name”)
@@ -16,14 +19,16 @@ urlpatterns = [
     #path('', views.index, name='root'), 
     #path('index/', views.index, name='index'),
     #path('detail/<int:id>',views.detail,name='detail'),
+    #path('articles/category/<int:category_id>',views.category,name='category'),
+    #path('articles/tag/<str:tag_name>',views.tag, name='tag')
     path('', ArticleListView.as_view(), name='root'),   # 使用通用视图    
     path('index/', ArticleListView.as_view(), name='index'),    # 使用通用视图
-    path('archive/',views.archive,name='archive'),
     path('about/',views.about,name='about'),
     path('detail/<int:pk>', ArticleDetailView.as_view(), name='detail'),   # 使用通用视图
     path('detail/new/', ArticleCreateView.as_view(), name='create'),   # 使用通用视图
     path('detail/<int:pk>/update/', ArticleUpdateView.as_view(), name='update'),   # 使用通用视图
     path('detail/<int:pk>/delete/', ArticleDeleteView.as_view(), name='delete'),   # 使用通用视图
-    path('articles/category/<int:category_id>',views.category,name='category'),
-    path('articles/tag/<str:tag_name>',views.tag, name='tag')
+    path('articles/user/<str:username>',UserArticleListView.as_view(),name='userarticlelist'),  # 使用通用视图
+    path('articles/category/<int:category_id>',CategoryListView.as_view(),name='category'),
+    path('articles/tag/<str:tag_name>',TagListView.as_view(), name='tag')
 ]  
--- DjangoBlog/blog/views.py
+++ DjangoBlog/blog/views.py
@@ -17,60 +17,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.contrib import messages
 
 # Create your views here.
-
-def index(request):
-    """
-    blog的首页
-    @param request
-    @return 'blog/index.html'
-    """ 
-    article_list = Article.objects.all().order_by('-created_date')[0:5]
-    for article in article_list:
-        article.picture = 'blog/image/article_picture/{}'.format(article.picture)    
-
-    context = {
-        'article_list': article_list
-    }
-    
-    return render(request, 'blog/index.html', context)
-
-def detail(request,id):
-    """
-    blog的detail页
-    @param request
-    @return 'blog/detail.html'
-    """ 
-    try:
-        article = Article.objects.get(id = id)
-        if article != None:
-            # 阅读量+1
-            article.viewed()
-            return render(request, 'blog/detail.html', {"article": article})
-    except:
-        return redirect(reverse('blog:index'))
-
-def category(request,category_id):
-    """
-    blog的articles页（安装Category分类后的博文列表）
-    @param request
-    @return 'blog/articles.html'
-    """ 
-    article_list = Article.objects.filter(category_id=category_id)
-    for article in article_list:
-        article.picture = 'blog/image/article_picture/{}'.format(article.picture)     
-    return render(request, 'blog/articles.html', {"article_list": article_list})
-
-def tag(request,tag_name):
-    """
-    blog的articles页（安装Category分类后的博文列表）
-    @param request
-    @return 'blog/articles.html'
-    """ 
-    article_list = Article.objects.filter(tag__name = tag_name) #__:表示查询tag字段的name中是否包含了tag_name的字符串
-    for article in article_list:
-        article.picture = 'blog/image/article_picture/{}'.format(article.picture)     
-    return render(request, 'blog/articles.html', {"article_list": article_list})
-
 def about(request):
     """
     blog的about页
@@ -79,17 +25,6 @@ def about(request):
     """
     output = '这是blog的about页面'
     return HttpResponse(output)
-
-@login_required
-def archive(request):
-    """
-    blog的archive页
-    @param request
-    @return 'blog/archive.html'
-    """    
-    output = '这是blog的archive页面'
-    return HttpResponse(output)
-
 
 # 使用通用视图，比起传统的函数方式，django对通用视图有很多后台处理
 # 只需要简单变量设置
@@ -145,3 +80,29 @@ class ArticleDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         if self.request.user == Article.author:            
             return True
         return False
+
+class UserArticleListView(ListView):
+    model = Article
+    template_name = 'blog/articles.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'article_list'    #模板中使用的上下文变量名
+
+    def get_queryset(self):
+        article_list= Article.objects.filter(author__username = self.kwargs['username']).order_by('-last_mod_date')
+        return article_list
+
+class CategoryListView(ListView):
+    model = Article
+    template_name = 'blog/articles.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'article_list'    #模板中使用的上下文变量名
+
+    def get_queryset(self):
+        article_list= Article.objects.filter(category_id = self.kwargs['category_id']).order_by('-last_mod_date')    
+        return article_list
+
+class TagListView(ListView):
+    model = Article
+    template_name = 'blog/articles.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'article_list'    #模板中使用的上下文变量名
+    def get_queryset(self):
+        article_list= Article.objects.filter(tag__name = self.kwargs['tag_name']).order_by('-last_mod_date')
+        return article_list
